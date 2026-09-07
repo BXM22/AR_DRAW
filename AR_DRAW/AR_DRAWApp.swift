ee1cc494e249c6e39a4405d0f836ed7e36a980f8
BXM22 <100313998+BXM22@users.noreply.github.com>
2026-09-06 18:39:17 -0700

    Initial Commit

---
+++ AR_DRAW/AR_DRAWApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_DRAWApp.swift
+//  AR_DRAW
+//
+//  Created by Brennen Meregillano on 9/6/26.
+//
+
+import SwiftUI
+
+@main
+struct AR_DRAWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR_DRAWUITests/AR_DRAWUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AR_DRAWUITestsLaunchTests.swift
+//  AR_DRAWUITests
+//
+//  Created by Brennen Meregillano on 9/6/26.
+//
+
+import XCTest
+
+final class AR_DRAWUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
